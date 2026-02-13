b1b5f6364dbf3beb66422c22a0198e4f42310e88
marciosobel <marciosobel@proton.me>
2026-02-13 19:35:39 -0300

    feat: add basic auth

---
+++ src/action.rs
@@ -0,0 +1,89 @@
+///! Source: https://github.com/airstrike/iced_receipts/blob/master/src/action.rs
+
+use iced::advanced::graphics::futures::MaybeSend;
+use iced::Task;
+use std::fmt;
+
+pub struct Action<I, Message> {
+    pub instruction: Option<I>,
+    pub task: Task<Message>,
+}
+
+impl<I, Message> Action<I, Message> {
+    /// Create a new `Action` with no `Instruction` or [`Task`](iced::Task).
+    pub fn none() -> Self {
+        Self {
+            instruction: None,
+            task: Task::none(),
+        }
+    }
+
+    /// Create a new `Action` with an `Instruction` and a [`Task`](iced::Task).
+    pub fn new(instruction: I, task: Task<Message>) -> Self {
+        Self {
+            instruction: Some(instruction),
+            task,
+        }
+    }
+
+    /// Create a new `Action` with an `Instruction` to be handled by some ancestor
+    /// component.
+    pub fn instruction(instruction: I) -> Self {
+        Self {
+            instruction: Some(instruction),
+            task: Task::none(),
+        }
+    }
+
+    /// Create a new `Action` with a [`Task`](iced::Task).
+    pub fn task(task: Task<Message>) -> Self {
+        Self {
+            instruction: None,
+            task,
+        }
+    }
+
+    /// Map the message of the `Action`'s [`Task`](iced::Task) to a different type.
+    pub fn map<N>(self, f: impl Fn(Message) -> N + MaybeSend + 'static) -> Action<I, N>
+    where
+        Message: MaybeSend + 'static,
+        N: MaybeSend + 'static,
+    {
+        Action {
+            instruction: self.instruction,
+            task: self.task.map(f),
+        }
+    }
+
+    /// Maps the `Instruction` of the `Action` to a different type.
+    pub fn map_instruction<N>(self, f: impl Fn(I) -> N + MaybeSend + 'static) -> Action<N, Message>
+    where
+        I: MaybeSend + 'static,
+        N: MaybeSend + 'static,
+    {
+        Action {
+            instruction: self.instruction.map(f),
+            task: self.task,
+        }
+    }
+
+    /// Sets the `Instruction` of an `Action`.
+    pub fn with_instruction(mut self, instruction: I) -> Self {
+        self.instruction = Some(instruction);
+        self
+    }
+
+    /// Sets the [`Task`](iced::Task) of an `Action`.
+    pub fn with_task(mut self, task: Task<Message>) -> Self {
+        self.task = task;
+        self
+    }
+}
+
+impl<Instruction: fmt::Debug, Message> fmt::Debug for Action<Instruction, Message> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Action")
+            .field("instruction", &self.instruction)
+            .finish()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-use iced::widget::{center, text};
+use opie::App;
 
 fn main() -> iced::Result {
     iced::application(App::new, App::update, App::view)
-        .theme(iced::theme::Theme::Dark)
+        .theme(App::theme)
+        .centered()
         .run()
-}
-
-struct App;
-
-#[derive(Debug, Clone)]
-enum Message {}
-
-impl App {
-    fn new() -> Self {
-        Self
-    }
-
-    fn update(&mut self, _message: Message) {}
-
-    fn view(&self) -> iced::Element<'_, Message> {
-        return center(text("Hello, opie!")).into();
-    }
 }
