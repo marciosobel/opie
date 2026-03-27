a5828c2a6e818b18b6c5c4f16e350d0f120f70f3
Márcio Sobel <marciosobel@proton.me>
2026-03-27 06:14:46 -0300

    chore: update `flake.nix`

--- flake.nix
+++ flake.nix
@@ -2,60 +2,43 @@
   description = "Rust development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
+    systems,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells = eachSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        # Read the file relative to the flake's root
-        overrides = builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml"));
+        pkgs = import nixpkgs {inherit system;};
       in {
-        devShells.default = pkgs.mkShell rec {
-          nativeBuildInputs = [pkgs.pkg-config];
+        default = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            clippy
+          ];
+
           buildInputs = with pkgs; [
-            clang
-            llvmPackages.bintools
-            rustup
+            openssl
+            sqlite
+            libxkbcommon
+            libGL
+            wayland
           ];
 
-          RUSTC_VERSION = overrides.toolchain.channel;
-
-          # https://github.com/rust-lang/rust-bindgen#environment-variables
-          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
-
-          shellHook = ''
-            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-          '';
-
-          # Add precompiled library to rustc search path
-          RUSTFLAGS = builtins.map (a: ''-L ${a}/lib'') [
-            # add libraries here (e.g. pkgs.libvmi)
-          ];
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
-
-          # Add glibc, clang, glib, and other headers to bindgen search path
-          BINDGEN_EXTRA_CLANG_ARGS =
-            # Includes normal include path
-            (builtins.map (a: ''-I"${a}/include"'') [
-              # add dev libraries here (e.g. pkgs.libvmi.dev)
-              pkgs.glibc.dev
-            ])
-            # Includes with special directory paths
-            ++ [
-              ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-              ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-              ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-            ];
+          LD_LIBRARY_PATH = builtins.toString (pkgs.lib.makeLibraryPath buildInputs);
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         };
       }
     );
+  };
 }
